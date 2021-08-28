f5d7b1dcb60dd44c1c10763c4909037ea02fbdc8
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2021-08-28 08:11:14 +0700

    Init commit

---
+++ TodoApiWithTrace/Tracing.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Trace;
+
+namespace TodoApiWithTrace
+{
+    public class AllTheTrace
+    {
+        public static void InstallTracingInServices(IServiceCollection services)
+        {
+            services.AddOpenTelemetryTracing(
+                (builder) => builder
+                    .AddAspNetCoreInstrumentation()
+                    .AddJaegerExporter()
+                    .AddConsoleExporter()
+                );
+        }
+    }
+}
